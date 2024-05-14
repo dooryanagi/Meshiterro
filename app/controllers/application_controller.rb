6ce4f55033241f98626cadb4a53e3ba1304f910c
EC2 Default User <ec2-user@ip-172-31-2-30.ap-northeast-1.compute.internal>
2024-05-14 02:13:38 +0000

    [Finish]4-6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  
+  # deviseのコントローラは直接修正できないため、全てのコントローラに対する処理を行える権限を持つこの場所に記述する
+  # before_actionにより、devise機能の前にメソッドが実行される
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  # privateと似た機能を持つが、protectedは他のコントローラーからも参照
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
