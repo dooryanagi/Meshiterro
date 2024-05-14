bb50fe3c117d00eb99cd2ccb33412488a183f72c
EC2 Default User <ec2-user@ip-172-31-2-30.ap-northeast-1.compute.internal>
2024-05-14 08:12:10 +0000

    [Finish]4-13

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,5 +1,5 @@
 class PostImagesController < ApplicationController
-  
+
   # 画像の新規投稿
   def new
     # form_withに渡すための空のモデルを用意
@@ -13,7 +13,7 @@ class PostImagesController < ApplicationController
   def show
     @post_image = PostImage.find(params[:id])
   end
-  
+
   # 投稿データの保存
   def create
     @post_image = PostImage.new(post_image_params)
@@ -22,12 +22,22 @@ class PostImagesController < ApplicationController
     @post_image.save
     redirect_to post_images_path
   end
-  
+
+  # 投稿データの削除
+  def destroy
+    # データを一件取得
+    post_image = PostImage.find(params[:id])
+    # データの削除
+    post_image.destroy
+    # 一覧へリダイレクト
+    redirect_to post_images_path
+  end
+
   # 投稿データのストロングパラメータを記述
   private
-  
+
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
-  
+
 end
