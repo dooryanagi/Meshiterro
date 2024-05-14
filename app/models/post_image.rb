61971ef25bd9a30df868ca4e1e97a1d547a140db
EC2 Default User <ec2-user@ip-172-31-2-30.ap-northeast-1.compute.internal>
2024-05-14 05:01:10 +0000

    [Finish]4-9

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,4 +3,8 @@ class PostImage < ApplicationRecord
   # 画像を持たせるための記述を追加
   has_one_attached :image
 
+  # １：NのNに相当する記述を追加する
+  # 画像側から見るとuserは常に1のため、単数形
+  belongs_to :user
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
   # パスワードの正確性を検証、ユーザ登録や編集削除、パスワードのリセット、ログイン情報の保存、emailのフォーマットなどのバリデーション
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  # 1：Nの関連付けを行う
+  # ユーザーと画像は、一人のユーザーに対してたくさんの画像となるため、ユーザーが1、画像がNとなる
+  has_many :post_image, dependent: :destroy
+  
 end
