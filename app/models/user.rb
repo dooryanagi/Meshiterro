4528cba82f046d1513cf145dd8b6a59e153b8e00
EC2 Default User <ec2-user@ip-172-31-2-30.ap-northeast-1.compute.internal>
2024-05-15 02:47:42 +0000

    [Finish]4-14

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,17 @@ class User < ApplicationRecord
   # ユーザーと画像は、一人のユーザーに対してたくさんの画像となるため、ユーザーが1、画像がNとなる
   has_many :post_image, dependent: :destroy
   
+  # userモデルに対してもActiveAtrageで画像を保存できるように設定する
+  has_one_attached :profile_image
+  
+  # プロフィール画像がなかった場合にサンプルを表示させる設定と大きさを変更できるメソッドを定義する
+  # 他でも呼び出せるよう、モデル内に定義する
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      profile_image.attach(io: File.open(file_path),filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.varient(resize_to_limit: [width, height]).processed
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
-
   # post_imagesコントローラーとアクションの作成によりできたルーティングをresourcesで書き換える
   # resourcesにonlyのオプションを追加することで、必要なルートみ作成できる
   resources :post_images, only: [:new, :index, :show, :create, :destroy]
+  
+  # usersコントローラーの作成とともにデフォルトで作成されたルーティングをresourcesで書き換える
+  resources :users, only: [:show, :edit]
 
   # モデルの作成時に自動生成
   # deviseを使用する際にURLとしてusersを含むことを示している
