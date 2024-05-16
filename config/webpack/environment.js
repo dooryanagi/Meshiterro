22a86ee2933dee0ba704ef77cb8149f4bdbf68a6
EC2 Default User <ec2-user@ip-172-31-2-30.ap-northeast-1.compute.internal>
2024-05-16 08:18:01 +0000

    [Finish]4-23

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+// Bootstrapを導入するために追記
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: 'popper.js'
+  })
+)
